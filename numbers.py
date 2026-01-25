003828bb4f73525958d5aed828410a4f75a7dc32
Nazym <nazymzeleu@gmail.com>
2026-01-25 17:31:28 +0500

    example5

---
+++ numbers.py
@@ -0,0 +1,20 @@
+a = 3   
+b = 2.9
+c = 2j   
+print(type(a))
+print(type(b))
+print(type(c))
+
+d = 35656222554887711
+e = -3255522
+
+print(type(d))
+print(type(e))
+
+x = 35e3
+y = 12E4
+z = -87.7e100
+
+print(type(x))
+print(type(y))
+print(type(z))
