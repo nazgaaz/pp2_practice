41738adf21d7c6cdb3a5380b348b1fdb9cf6400c
Zheleu Nazym <nazymzeleu@gmail.com>
2026-01-29 20:18:31 +0500

    Add files via upload

---
+++ PP2_Practice/Practice1/strings.py
@@ -0,0 +1,13 @@
+a = "Hello, my teacher!"
+print(a)
+print(a[:5])
+print(a.lower())
+print(a.strip())
+print(a.replace("H", "J"))
+print(a.split(","))
+b="hello"
+c='world'
+print(b +" " + c )
+age = 18
+txt = f"My name is Nazym, I am {age}"
+print(txt)
---
+++ PP2_Practice/Practice1/variables.py
@@ -0,0 +1,13 @@
+x = 11
+y = "Nazgaz"
+print(x)
+print(y)
+
+x = 4       # x is of type int
+x = "Di" # x is now of type str
+print(x)
+
+x = 5
+y = "Aru"
+print(type(x))
+print(type(y))
